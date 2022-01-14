39b6e9b0f3d869ca36feeb1b9b8e9f841b4a2325
LeATeP <leateps@gmail.com>
2022-01-14 09:57:27 +0400

    second socket's commit, still in dev

--- socket_utils/client_conn.py
+++ socket_utils/client_conn.py
@@ -1,16 +1,23 @@
 #!/bin/python3
 
 import socket
+from threading import Thread
 
-addr = ('172.18.0.4', 8080)
 
-sock = socket.socket()
-sock.connect(addr)
 
-while True:
-    msg = bytes(input(), 'utf-8')
-    sock.sendall(msg)
+class conn_to_main_server:
+    def __init__(self) -> None:
+        server_ip = ''
+        addr = (server_ip, 9999)
 
-    data = sock.recv(1024)
-    if data:
-        print(data)
+        self.sock = socket.socket()
+        self.sock.connect(addr)
+
+        unit_id = self.sock.recv(1024)
+        if unit_id:
+            Thread(target=self.wait_signals).start()
+
+
+    def wait_signals(self):
+        # get unit_id to associate yourself with
+        pass
--- socket_utils/main_manager.py
+++ socket_utils/main_manager.py
@@ -1,22 +1,67 @@
 #!/bin/python3
 
 import socket
+from threading import Thread
+from os import environ
+from sys import path
+
+env = environ.get
+path.append(env("vaava"))
+from sql.sql_query import psql
 
 
 
-sock = socket.socket()
-addr = ('0.0.0.0', 8080)  # ip will change to 'main_server' after main_server container is created
-sock.bind(addr)
+class Manager(psql):
+    def __init__(self) -> None:
+        psql.__init__(self)
 
-print(f'listening to {addr}')
-sock.listen()
-conn, addr = sock.accept()
+        self.exec('''
+                  update unit 
+                  set state = 'idle', container_id = null 
+                  where fraction = 'Main';''')
+        
+        self.init_server()
 
-print(f"Connected by: {addr}")
-while True:
-    data = conn.recv(1024)
-    if data:
-        print(f"received {data} \nsending back response...")
-        msg_back = f"You send: {data}"
-        msg_bytes = bytes(msg_back, 'utf-8')
-        conn.sendall(b"received")
+    def init_server(self) -> None:
+        sock = socket.socket()
+        addr = ('0.0.0.0', 9999)  # ip will change to 'main_server' after main_server container is created
+        sock.bind(addr)
+        
+        print(f'listening to {addr}')
+        sock.listen()
+
+        while True:
+            conn, addr = sock.accept()
+            print(f"Connected by: {addr}")
+            Thread(target=self.create_thread, args=(conn,)).start()
+            # creating thread, for new client
+            
+    def create_thread(self, conn):
+        check_available_unit = self.exec('''
+                                        select id from unit 
+                                        where state = 'idle' and fraction = 'Main'
+                                        limit 1;''')
+        unit = self.fetch_dict()
+        print(unit)
+        if unit:
+            id = str(unit[0]['id'])
+            encoded = bytes(id, 'utf-8')
+            conn.sendall(encoded)
+        else:
+            no_msg = bytes('no', 'utf-8')
+            conn.sendall(no_msg)
+            
+        # while True:
+            # data = conn.recv(1024)
+            # if data:
+                
+
+Manager()
+    
+# while True:
+    # data = conn.recv(1024)
+    # if data:
+        # print(f"received {data} \nsending back response...")
+        # msg_back = f"You send: {data}"
+        # msg_bytes = bytes(msg_back, 'utf-8')
+        # conn.sendall(b"received")
